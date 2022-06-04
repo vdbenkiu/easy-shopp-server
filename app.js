e0bdc667bd8af2d93d6baf4487ff12a517a0aa14
vdbenkiu <91454567+vdbenkiu@users.noreply.github.com>
2022-06-05 00:04:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -70,8 +70,12 @@ mongoose.connect(process.env.CONNECT_STRING,
 
 
 // production
-var server= app.listen(process.env.PORT || 3000 , function () {
-  var port = server.address().port;
-  console.log("Express is working on port"+ port)
-})
-
+// var server= app.listen(process.env.PORT || 3000 , function () {
+//   var port = server.address().port;
+//   console.log("Express is working on port"+ port)
+// })
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
